74f30a8c4e48c2f924d3f2413d463abd0443d463
Radha <goyalradha2001@gmail.com>
2025-03-31 10:33:04 +0530

    navigation challenge corrected

--- JS DOM challenges 2/challenge-9/app.js
+++ JS DOM challenges 2/challenge-9/app.js
@@ -1,3 +1,9 @@
-/**
- * Write your challenge solution here
- */
+
+const nav_btn = document.querySelector(".close-btn");
+const nav_open_btn = document.querySelector(".open-btn");
+nav_btn.addEventListener("click",()=>{
+  document.querySelector(".menu").style.transform = "translateX(100%)";
+})
+nav_open_btn.addEventListener("click",()=>{
+  document.querySelector(".menu").style.transform = "translateX(0)";       
+})
