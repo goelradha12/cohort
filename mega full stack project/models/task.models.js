576d1e931294f2d5eafee7f785a8846ce3245e78
Radha <goyalradha2001@gmail.com>
2025-05-02 17:18:11 +0530

    Updated format

--- mega full stack project/models/task.models.js
+++ mega full stack project/models/task.models.js
@@ -1,47 +1,50 @@
 import mongoose, { Schema } from "mongoose";
 import { AvailableTaskStatus, TaskStatusEnum } from "../utils/constants";
 
-const taskSchema = new Schema({
-    title : {
-        type : String,
-        required : true,
-        trim : true,
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    description : {
-        type: String,
+    description: {
+      type: String,
     },
-    project : {
-        type: Schema.Types.ObjectId,
-        ref : "Project",
-        required : true
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
     },
-    assignedTo : {
-        type : Schema.Types.ObjectId,
-        ref : "User",
+    assignedTo: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    assignedBy : {
-        type : Schema.Types.ObjectId,
-        ref : "User",
+    assignedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    status : {
-        type : String,
-        enum : AvailableTaskStatus,
-        default : TaskStatusEnum.TODO,
+    status: {
+      type: String,
+      enum: AvailableTaskStatus,
+      default: TaskStatusEnum.TODO,
     },
-    attachments : {
-        type : [
-            {
-                url : String,
-                mimetype : String,
-                size : Number
-            }
-        ], 
-        default : []
-    }
-},{
-    timestamps : true 
-})
+    attachments: {
+      type: [
+        {
+          url: String,
+          mimetype: String,
+          size: Number,
+        },
+      ],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const Task = mongoose.model("Task",taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
-export {Task};
+export { Task };
