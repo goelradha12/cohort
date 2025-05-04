7956a219b508dfc1e284cd616035c22935d06d74
Radha <goyalradha2001@gmail.com>
2025-05-04 14:15:15 +0530

    Docker installed and connected successfully

--- mega full stack project/db/index.js
+++ mega full stack project/db/index.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 
 
-dotenv.config({path:"../.env"})
+// dotenv.config({path:"./.env"})
+
+// const MONGO_URI = "mongodb://localhost:27017/mydatabase";
 
 const db = async()=>{
     try {
--- mega full stack project/src/index.js
+++ mega full stack project/src/index.js
@@ -1,9 +1,8 @@
-import app from "./app.js"
+import app from "../app.js"
 import dotenv from "dotenv"
-import connectDb from "./db/index.js"
+import connectDb from "../db/index.js"
 
 dotenv.config({path:"./.env"})
-
 const PORT = process.env.PORT || 3000;
 
 connectDb()
