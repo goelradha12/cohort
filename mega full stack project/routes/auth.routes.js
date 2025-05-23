e82c0c6a2d11d4811739a7a9efd96fa2d3ac1637
Radha <goyalradha2001@gmail.com>
2025-05-23 23:43:44 +0530

    email verification controller completed successfully

--- mega full stack project/routes/auth.routes.js
+++ mega full stack project/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express"
 import { validate } from "../middlewares/validator.middleware.js"
-import { registerUser } from "../controllers/auth.controllers.js"
+import { registerUser, verifyMail } from "../controllers/auth.controllers.js"
 import { userRegistrationValidator } from "../validators/index.js"
 
 const router = Router();
 
 router.route("/register").post(userRegistrationValidator(),validate,registerUser);
-
+router.route("/verifyMail/:token").get(verifyMail);
 export default router;
