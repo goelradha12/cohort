d6ac568352f1b7e387bfd0740eed4503c0f014fc
Radha <goyalradha2001@gmail.com>
2025-05-24 15:36:49 +0530

    User Change Password completed Successfully

--- mega full stack project/routes/auth.routes.js
+++ mega full stack project/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { validate } from "../middlewares/validator.middleware.js"
-import { loginUser, registerUser, verifyMail } from "../controllers/auth.controllers.js"
-import { userRegistrationValidator } from "../validators/index.js"
+import { changeCurrPassword, loginUser, registerUser, verifyMail } from "../controllers/auth.controllers.js"
+import { userChangePasswordValidator, userLoginValidator, userRegistrationValidator } from "../validators/index.js"
 
 const router = Router();
 
 router.route("/register").post(userRegistrationValidator(),validate,registerUser);
 router.route("/verifyMail/:token").get(verifyMail);
-router.route("/login").post(loginUser);
+router.route("/login").post(userLoginValidator(),validate,loginUser);
+router.route("/changePassword").post(userChangePasswordValidator(),validate,changeCurrPassword);
 export default router;
