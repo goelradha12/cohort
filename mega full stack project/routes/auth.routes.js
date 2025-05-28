6a38375c1c554b535c0886da8c3870416115f977
Radha <goyalradha2001@gmail.com>
2025-05-28 14:46:13 +0530

    File (avatar) upload feature integrated

--- mega full stack project/routes/auth.routes.js
+++ mega full stack project/routes/auth.routes.js
@@ -3,6 +3,7 @@ import { validate } from "../middlewares/validator.middleware.js"
 import { changeCurrPassword, forgotPasswordRequest, getUser, loginUser, logOutUser, refreshAccessToken, registerUser, resendVerificationEmail, resetPassword, updateUserProfile, verifyMail } from "../controllers/auth.controllers.js"
 import { forgotPasswordRequestValidator, resendVerificationEmailValidator, resetPasswordValidator, userChangePasswordValidator, userLoginValidator, userRegistrationValidator } from "../validators/index.js"
 import { isLoggedIn } from "../middlewares/auth.middlewares.js";
+import upload from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
@@ -16,5 +17,5 @@ router.route("/resetPassword/:token").post(resetPasswordValidator(),validate,res
 router.route("/getProfile").get(isLoggedIn,getUser);
 router.route("/logout").get(logOutUser);
 router.route("/refreshAccessToken").get(refreshAccessToken);
-router.route("/updateProfile").post(isLoggedIn,updateUserProfile);
+router.route("/updateProfile").post(isLoggedIn,upload.single('newAvatar'), updateUserProfile);
 export default router;
