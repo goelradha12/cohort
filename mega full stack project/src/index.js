6aa0b65a13bce69a92eb7414dccfa2bf5592796d
Radha <goyalradha2001@gmail.com>
2025-05-28 08:14:52 +0530

    Project controllers started

--- mega full stack project/src/index.js
+++ mega full stack project/src/index.js
@@ -5,12 +5,13 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import authRouter from "../routes/auth.routes.js"
-
+import projectRouter from "../routes/project.routes.js"
 
 dotenv.config({path:"./.env"})
 app.use(express.json());
 app.use(cors({
-    origin: process.env.BASE_URL
+    origin: process.env.BASE_URL,
+    credentials: true
 }))
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
@@ -19,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDb()
 .then(
-    app.listen(PORT,(req,res)=>{
+    app.listen(PORT,()=>{
         console.log("I am listening here: ",PORT);
     })
 )
@@ -30,3 +31,4 @@ app.get("/",(req,res)=>
     console.log(req.body)
 })
 app.use("/api/v1/users",authRouter)
+app.use("/project",projectRouter)
