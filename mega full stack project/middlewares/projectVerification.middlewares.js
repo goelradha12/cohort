5b5dcb7f286ed5bfcfabf8f0457374d1cd961170
Radha <goyalradha2001@gmail.com>
2025-06-02 11:50:34 +0530

    project notes and project member controllers added without verification

--- mega full stack project/middlewares/projectVerification.middlewares.js
+++ mega full stack project/middlewares/projectVerification.middlewares.js
@@ -52,7 +52,7 @@ export const verifyProjectAccess = (roles = []) => asyncHandler(async (req, res,
     // has a role (mentioned in array) for the project
     try {
         const projectID = new mongoose.Types.ObjectId(`${req.params.projectID}`);
-
+        console.log(projectID)
         if (!projectID)
             throw new apiError(401, "ProjectID is required")
         // get role from userID and projectID
@@ -61,13 +61,15 @@ export const verifyProjectAccess = (roles = []) => asyncHandler(async (req, res,
             project: projectID
         }).select(['role'])
 
-        
+        if(!UserRole)
+            throw new apiError(401,"Access Denied")
         // check if that role is there in 
         const isVerfied = roles.includes(UserRole.role)
         // console.log(UserRole.role,roles);
         console.log(isVerfied)
         if (isVerfied)
         {
+            console.log("verified")
             next();
         }
         else
@@ -87,5 +89,32 @@ export const verifyProjectAccess = (roles = []) => asyncHandler(async (req, res,
             success: false,
             message: "Something went wrong",
         })
+    }
+})
+
+export const verifyMember = asyncHandler(async (req,res,next) => {
+    // Goal: take memberID from params and check if it is valid member
+    const {memberID,projectID} = req.params;
+    try {
+        
+        const myMember = ProjectMember.findById(memberID);
+        if(myMember) next();
+        else throw new apiError(401,"Invalid Member")
+        
+    } catch (error) {
+       console.log(error);
+        if (error instanceof apiError) {
+        return res.status(error.statusCode).json({
+            statusCode: error.statusCode,
+            message: error.message,
+            success: false,
+        });
+        }
+
+        return res.status(500).json({
+        statusCode: 500,
+        message: "Something went wrong",
+        success: false,
+        }); 
     }
 })
