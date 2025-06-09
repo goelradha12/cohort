58e897553cfdd911e35b5135badb72dd3cf9737d
Radha <goyalradha2001@gmail.com>
2025-06-09 18:48:32 +0530

    verified and updated register and verifyemail controllers

--- MyLeetlab/backend/src/middlewares/auth.middlewares.js
+++ MyLeetlab/backend/src/middlewares/auth.middlewares.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import { apiError } from "../utils/api.error.js";
+import bcrypt from "bcryptjs"
 
-
-export const isLoggedIn = (req,res,next) => {
+export const isLoggedIn = (req, res, next) => {
     // send next() only if valid access token
 
     const token = req.cookies.accessToken;
@@ -27,4 +27,18 @@ export const isLoggedIn = (req,res,next) => {
             success: false
         });
     }
+}
+
+export const hashPassword = async (params, next) => {
+    if (params.model == 'User' && (params.action == 'create' || params.action == 'update')) {
+        const data = params.args.data;
+
+        if (data?.password) {
+            const hashedPassword = await bcrypt.hash(data.password, 10);
+            data.password = hashedPassword;
+        }
+    }
+
+    return next(params);
+    // Logic only runs for delete action and Post model
 }
