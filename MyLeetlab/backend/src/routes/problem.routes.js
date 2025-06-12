f91b9169f22965285909a690704427308665c29a
Radha <goyalradha2001@gmail.com>
2025-06-13 00:59:37 +0530

    adding validators for problem controller

--- MyLeetlab/backend/src/routes/problem.routes.js
+++ MyLeetlab/backend/src/routes/problem.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { checkAdmin, isLoggedIn, isVerified } from "../middlewares/auth.middlewares.js"
 import { createProblem, deleteProblem, getAllProblems, getAllProblemsSolvedByUser, getProblemByID, updateProblem } from "../controllers/problem.controllers.js"
+import { validate } from "../middlewares/validator.middleware.js"
+import { createProblemValidator } from "../validators/problem.validators.js"
 
 const router = express.Router()
 
 router.route("/").get(isLoggedIn,isVerified,getAllProblems)
-router.route("/create-problem").post(isLoggedIn,checkAdmin,createProblem)
+router.route("/create-problem").post(isLoggedIn,checkAdmin,createProblemValidator(),validate,createProblem)
 router.route("/:id").get(isLoggedIn,isVerified,getProblemByID)
         .put(isLoggedIn,checkAdmin,updateProblem)
         .delete(isLoggedIn,checkAdmin,deleteProblem)
