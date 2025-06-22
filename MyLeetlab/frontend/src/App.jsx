31f98d4177209ccf8f3d573e47d4381f8a9fc312
Radha <goyalradha2001@gmail.com>
2025-06-22 18:24:19 +0530

    Added toaster and signup stores

--- MyLeetlab/frontend/src/App.jsx
+++ MyLeetlab/frontend/src/App.jsx
@@ -3,12 +3,15 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./page/LoginPage";
 import SignUpPage from "./page/SignUpPage";
 import HomePage from "./page/HomePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   // giving useful routes to the user as per the authentication
   let authUser = null;
   return (
     <>
+
+      <Toaster/>
       <Routes>
           <Route
             path="/"
