9bc3622635480fb68a8453f7fd0380879018ec3e
Radha <goyalradha2001@gmail.com>
2025-06-24 19:06:08 +0530

    Added form template and form validator successfully

--- MyLeetlab/frontend/src/page/AddProblem.jsx
+++ MyLeetlab/frontend/src/page/AddProblem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CreateProblemForm from '../components/CreateProblemForm'
 
 const AddProblem = () => {
   return (
     <div>
       Add Problem
+      <CreateProblemForm />
     </div>
   )
 }
