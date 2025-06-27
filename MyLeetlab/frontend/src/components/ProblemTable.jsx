3adc7e3047fb4ef10c617733eae35fa5f674571c
Radha <goyalradha2001@gmail.com>
2025-06-27 23:48:59 +0530

    Displaying problems in home page as per filters

--- MyLeetlab/frontend/src/components/ProblemTable.jsx
+++ MyLeetlab/frontend/src/components/ProblemTable.jsx
@@ -1,8 +1,8 @@
 import React, { useMemo, useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Plus } from "lucide-react";
+import { Bookmark, PencilIcon, Plus, TrashIcon } from "lucide-react";
 
-const ProblemTable = ({ problems }) => {
+const ProblemTable = ({ problems, solvedProblems }) => {
     const { authUser } = useAuthStore();
 
     const [search, setSearch] = useState("");
@@ -45,6 +45,17 @@ const ProblemTable = ({ problems }) => {
         return filteredProblems.slice(startIndex, endIndex);
     }, [filteredProblems, currentPage]); // problems to display
 
+    // handling admin action buttons
+    const handleDelete = (problemId) => {
+        console.log("Delete button", problemId);
+    }
+    const handleAddToPlaylist = (problemId) => {
+        console.log("Add to playlist button", problemId);
+
+    }
+    const handleEdit = (problemId) => {
+        console.log("Edit button", problemId);
+    }
     return (
         <div className="w-full max-w-6xl mx-auto mt-10">
             <div className="flex justify-between items-center mb-6">
@@ -101,6 +112,69 @@ const ProblemTable = ({ problems }) => {
                             <th>Actions</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        {console.log(currentProblems, problems, filteredProblems, solvedProblems)}
+                        {currentProblems.length === 0 && <tr><td colSpan="5" className="text-center">No problems found</td></tr>}
+                        {currentProblems.map((problem) => {
+                            const isSolved = solvedProblems.map((p) => p.id).includes(problem.id)
+                            return (
+                                <tr key={problem.id}>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            checked={isSolved}
+                                            readOnly
+                                            className="checkbox checkbox-sm outline-1 outline-primary"
+                                        />
+
+                                    </td>
+                                    <td>{problem.title}</td>
+                                    <td>
+                                        {problem.tags.map((tag) => (
+                                            <span key={tag} className="badge border-1 border-primary mr-2">
+                                                {tag}
+                                            </span>
+                                        ))}
+                                    </td>
+                                    <td><span
+                                        className={`badge font-semibold text-xs text-white ${problem.difficulty === "EASY"
+                                            ? "badge-success"
+                                            : problem.difficulty === "MEDIUM"
+                                                ? "badge-warning"
+                                                : "badge-error"
+                                            }`}
+                                    >
+                                        {problem.difficulty}
+                                    </span></td>
+                                    <td>
+                                        <div className="flex flex-col md:flex-row gap-2 items-start md:items-center">
+                                            {authUser?.role === "ADMIN" && (
+                                                <div className="flex gap-2">
+                                                    <button
+                                                        onClick={() => handleDelete(problem.id)}
+                                                        className="btn btn-sm btn-error"
+                                                    >
+                                                        <TrashIcon className="w-4 h-4 text-white" />
+                                                    </button>
+                                                    <button onClick={() => handleEdit(problem.id)} className="btn btn-sm btn-warning">
+                                                        <PencilIcon className="w-4 h-4 text-white" />
+                                                    </button>
+                                                </div>
+                                            )}
+                                            <button
+                                                className="btn btn-sm btn-outline flex gap-2 items-center"
+                                                onClick={() => handleAddToPlaylist(problem.id)}
+                                            >
+                                                <Bookmark className="w-4 h-4" />
+                                                <span className="hidden sm:inline">Save to Playlist</span>
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                        )}
+                    </tbody>
                 </table>
             </div>
         </div>
