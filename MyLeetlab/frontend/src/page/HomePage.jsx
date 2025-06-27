67577f261eaa37b5c748adc4fce6eecb467b2c83
Radha <goyalradha2001@gmail.com>
2025-06-27 19:21:10 +0530

    Showing all problems on home page

--- MyLeetlab/frontend/src/page/HomePage.jsx
+++ MyLeetlab/frontend/src/page/HomePage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import { useAuthStore } from '../store/useAuthStore';
+import React, { useEffect } from 'react'
+import { useAuthStore } from '../store/useAuthStore.js';
+import { useProblemStore } from '../store/useProblemStore.js';
+import { Loader } from 'lucide-react';
+import ProblemTable from '../components/ProblemTable';
+
 
 const HomePage = () => {
   const { authUser } = useAuthStore();
+  const { problems, getAllProblem, isProblemsLoading } = useProblemStore();
+
+  useEffect(
+    () => { getAllProblem() }
+    , [getAllProblem])
+
+  console.log(problems)
+  if (isProblemsLoading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
+
+
   return (
     <div className="min-h-screen flex flex-col items-center mt-14 px-4">
       <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
@@ -15,6 +35,15 @@ const HomePage = () => {
         interviews and helps you to improve your coding skills by solving coding
         problems
       </p>
+
+      {
+        problems.length ?
+          <ProblemTable problems={problems} />
+          :
+          <p className='mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed'>
+            No Problem Found
+          </p>
+      }
     </div>
   )
 }
