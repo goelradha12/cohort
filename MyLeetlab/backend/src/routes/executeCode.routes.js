362f126f70be03134f7c7d751778a028ac27cb1b
Radha <goyalradha2001@gmail.com>
2025-06-29 12:31:35 +0530

    Added route + controller for run code without submitting

--- MyLeetlab/backend/src/routes/executeCode.routes.js
+++ MyLeetlab/backend/src/routes/executeCode.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isLoggedIn, isVerified } from "../middlewares/auth.middlewares.js";
-import { executeCode } from "../controllers/executeCode.controllers.js";
+import { executeCode, runCode } from "../controllers/executeCode.controllers.js";
 import { executeCodeValidator } from "../validators/executeCode.validators.js";
 import { validate } from "../middlewares/validator.middleware.js";
 
 const router = express.Router();
 
 router.route("/").post(isLoggedIn, isVerified, executeCodeValidator(), validate, executeCode)
+router.route("/run").post(isLoggedIn, isVerified, executeCodeValidator(), validate, runCode)
 
 export default router;
