d6549b4f70c7dbd00eee491f98829a78096cc87f
Radha <goyalradha2001@gmail.com>
2025-06-29 17:25:14 +0530

    Added code execution logic successfully

--- MyLeetlab/frontend/src/page/ProblemPage.jsx
+++ MyLeetlab/frontend/src/page/ProblemPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams, useNavigate } from "react-router";
 import { useProblemStore } from "../store/useProblemStore.js";
 import {
@@ -19,6 +19,8 @@ import {
 import { useAuthStore } from "../store/useAuthStore.js";
 import Editor from '@monaco-editor/react';
 import { EditorOptions } from "../components/EditorOptions.js";
+import { useExecuteCodeStore } from "../store/useExecuteCodeStore.js";
+import { getJudge0LanguageID } from "../lib/utilFunctions.js";
 
 const ProblemPage = () => {
     const navigate = useNavigate();
@@ -35,7 +37,31 @@ const ProblemPage = () => {
     const [editorTheme, setEditorTheme] = useState("vs-dark");
     const [code, setCode] = useState("");
     const [activeTab, setActiveTab] = useState("description");
+    const [testcases, setTestcases] = useState([]);
+    const { isExecutingCode, executionResult, executeCode } = useExecuteCodeStore();
 
+    useEffect(() => {
+        console.log(problem);
+        if (problem) {
+            setSelectedLanguage(Object.keys(problem.codeSnippets)[0])
+            setCode(Object.values(problem.codeSnippets)[0]);
+            setTestcases(problem.testcases);
+        }
+    }, [problem])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try {
+            const language_id = getJudge0LanguageID(selectedLanguage);
+            const stdin = problem.testcases.map((tc) => tc.input);
+            const expected_outputs = problem.testcases.map((tc) => tc.output);
+            // console.log("data sent: ", { code, language_id, stdin, expected_outputs, id, selectedLanguage });
+            executeCode(code, language_id, stdin, expected_outputs, id);
+            // console.log(executionResult)
+        } catch (error) {
+            console.log("Error executing code", error);
+        }
+    }
     const renderTabContent = () => {
         switch (activeTab) {
             case "description":
@@ -43,15 +69,15 @@ const ProblemPage = () => {
                     <div className="prose max-w-none">
                         <div className="flex items-center justify-between gap-2 pb-6">
 
-                        <p className="font-semibold">{problem.difficulty}</p>
-                        <span>
-                            {problem.tags.map((tag)=>(
-                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    {tag}
-                                </span>
-                            ))}
-                        </span>
-                            </div>
+                            <p className="font-semibold">{problem.difficulty}</p>
+                            <span>
+                                {problem.tags.map((tag, i) => (
+                                    <span key={i} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </span>
+                        </div>
                         <p className="text-lg mb-6">{problem.description}</p>
 
                         {problem.examples && (
@@ -75,7 +101,7 @@ const ProblemPage = () => {
                                                 <div className="text-indigo-500 mb-2 text-base font-semibold">
                                                     Output:
                                                 </div>
-                                                <span className= "pb-1 rounded-lg font-semibold">
+                                                <span className="pb-1 rounded-lg font-semibold">
                                                     {example.output}
                                                 </span>
                                             </div>
@@ -135,9 +161,6 @@ const ProblemPage = () => {
         );
     }
 
-    {
-        console.log(problem);
-    }
     return (
         <div className="min-h-screen bg-gradient-to-br from-base-300 to-base-200 w-full">
             <nav className="container mx-auto p-4">
@@ -183,11 +206,14 @@ const ProblemPage = () => {
                             id="selectedLanguage"
                             value={selectedLanguage}
                             defaultValue={Object.keys(problem.codeSnippets)[0]}
-                            onChange={(e) => setSelectedLanguage(e.target.value)}
+                            onChange={(e) => {
+                                setSelectedLanguage(e.target.value)
+                                setCode(problem.codeSnippets[e.target.value])  // updating editor code as well
+                            }}
                         >
                             {/* get languages from problem data */}
                             {Object.keys(problem.codeSnippets).map((language) => {
-                                return <option value={language}>{language}</option>;
+                                return <option key={language} value={language}>{language}</option>;
                             })}
                         </select>
                         <select name="EditorTheme" className="select select-primary" id="editorTheme" value={editorTheme} onChange={(e) => setEditorTheme(e.target.value)}>
@@ -202,7 +228,7 @@ const ProblemPage = () => {
             <div className="container mx-auto p-4">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                     {/* COl 1 */}
-                    <div className="card bg-base-100 shadow-xs">
+                    <div className="card bg-base-100 shadow-xl h-[82vh] overflow-scroll">
                         <div className="card-body p-0">
                             {/* Tabs buttons */}
                             <div className="tabs tabs-bordered border-b-1 border-b-gray-200">
@@ -241,7 +267,7 @@ const ProblemPage = () => {
                         </div>
                     </div>
                     {/* COl 2 */}
-                    <div className="card bg-base-100 shadow-xs">
+                    <div className="card bg-base-100 shadow-xl">
                         <div className="card-body p-0">
                             <div className="tabs tabs-bordered">
                                 <button className="tab tab-active gap-2">
@@ -249,23 +275,32 @@ const ProblemPage = () => {
                                     Code Editor
                                 </button>
                             </div>
-                            <div className="h-[600px] w-full">
+                            <div className="h-[70vh] w-full">
                                 < Editor
                                     height="100%"
+                                    value={code}
+                                    theme={editorTheme}
                                     language={selectedLanguage.toLowerCase()}
                                     options={EditorOptions}
-                                    theme={editorTheme}
-                                    value={code}
                                     onChange={(value) => setCode(value || "")}
+                                    automaticLayout={true}
                                 />
                             </div>
-                            <div className="p-4 border-t border-base-300 bg-base-200">
+                            <div className="pb-5 px-2 border-t border-base-300 bg-base-200">
                                 <div className="flex justify-between items-center">
                                     <button
                                         className={`btn btn-primary gap-2`}
-                                        onClick={() => { console.log("Submitting the code") }}
+                                        onClick={(e)=>handleSubmit(e)}
+                                        disabled={isExecutingCode}
                                     >
-                                        Submit Solution
+                                        {isExecutingCode ?
+                                            <>
+                                                <Loader className="w-4 h-4" />
+                                                <span>Submitting...</span>
+                                            </>
+                                            :
+                                            `Submit Solution`}
+
                                     </button>
                                     <button disabled className="btn btn-success gap-2">
                                         Run Code
@@ -276,7 +311,34 @@ const ProblemPage = () => {
                     </div>
                 </div>
             </div>
-        </div>
+
+            {/* Editor and content ended ---------- Result + testcase area */}
+            <div className="container mx-auto card bg-base-100 shadow-xl my-6">
+                <div className="card-body">
+                    <div className="flex items-center justify-between mb-6">
+                        <h3 className="text-lg font-bold">Test Cases</h3>
+                    </div>
+                    <div>
+                        <table className="table table-zebra w-full">
+                            <thead>
+                                <tr>
+                                    <th>Input</th>
+                                    <th>Expected Output</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {testcases.map((testCase, index) => (
+                                    <tr key={index}>
+                                        <td className="font-mono">{testCase.input}</td>
+                                        <td className="font-mono">{testCase.output}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
