a4a56b35f640c753c6657e15faaeff61e1069cf7
Radha <goyalradha2001@gmail.com>
2025-07-01 19:05:16 +0530

    Added 404 page

---
+++ MyLeetlab/frontend/src/page/PageNotFound.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
+const PageNotFound = () => {
+    const navigation = useNavigate();
+    return (
+        <div className="h-screen w-screen flex items-center">
+            <div className="container flex flex-col md:flex-row items-center justify-center">
+                <div className="max-w-md text-center grid gap-2">
+                    <div className="text-5xl font-bold">404</div>
+                    <p className="text-2xl md:text-3xl font-light leading-normal">
+                        Sorry we couldn't find this page.{" "}
+                    </p>
+                    <p className="mb-8">
+                        But dont worry, you can find plenty of other things on our homepage.
+                    </p>
+                    <button onClick={()=> navigation("/")} className="btn text-lg btn-primary">
+                        Back to Homepage
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PageNotFound;
