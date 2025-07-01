daa2ed28ed1bc239b2718d82ee2f16d3984caf87
Radha <goyalradha2001@gmail.com>
2025-07-02 01:24:54 +0530

    Made problem page content dynamic and added a new controller

--- MyLeetlab/backend/src/routes/submission.routes.js
+++ MyLeetlab/backend/src/routes/submission.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { isLoggedIn, isVerified } from "../middlewares/auth.middlewares.js";
-import { getAllSubmission, getSubmissionCount, getSubmissionForProblem } from "../controllers/submission.controllers.js";
+import { getAllSubmission, getSubmissionCount, getSubmissionForProblem, getSuccessfulSubmissionCount } from "../controllers/submission.controllers.js";
 
 const router = Router();
 
 router.route("/").get(isLoggedIn, isVerified, getAllSubmission);
 router.route("/get-submission-count/:problemId").get(isLoggedIn, isVerified, getSubmissionCount)
+router.route("/success-submission-count/:problemId").get(isLoggedIn, isVerified, getSuccessfulSubmissionCount)
 router.route("/:problemId").get(isLoggedIn, isVerified, getSubmissionForProblem)
 export default router;
