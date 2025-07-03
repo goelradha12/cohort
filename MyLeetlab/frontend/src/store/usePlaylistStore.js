0dd5f1d1650541fa5062e529e44b34932a8b794a
Radha <goyalradha2001@gmail.com>
2025-07-03 15:49:19 +0530

    Component folder improved and added feature to view each playlist in user profile

--- MyLeetlab/frontend/src/store/usePlaylistStore.js
+++ MyLeetlab/frontend/src/store/usePlaylistStore.js
@@ -24,42 +24,34 @@ export const usePlaylistStore = create((set)=> ({
 
     addProblemToPlaylist: async (playlistId, problemIds) => {
         try {
-            set({isFetchingPlaylist: true});
             const response = await axiosInstance.post(`/playlist/${playlistId}/add-problem`, {problemIds});
             toast.success(response?.data?.message || "Problem added")
         } catch (error) {
             console.log("Error fetching playlists: ",error);
             toast.error(error.response?.data?.message || "Error Adding Problem")
-        } finally {
-            set({isFetchingPlaylist: false});
         }
     },
 
     removeProblemFromPlaylist: async (playlistId, problemIds) => {
         try {
-            set({isFetchingPlaylist: true});
+            console.log(problemIds)
             const response = await axiosInstance.delete(`/playlist/${playlistId}/remove-problem`,{problemIds});
             toast.success(response?.data?.message || "Problem removed")
         } catch (error) {
             console.log("Error fetching playlists: ",error);
             toast.error(error.response?.data?.message || "Error Removing Problem")
-        } finally {
-            set({isFetchingPlaylist: false});
-        }
+        } 
     },
 
     createNewPlaylist: async (data) => {
         try {
-            set({isFetchingPlaylist: true});
             const response = await axiosInstance.post("/playlist", data);
             set({playlist: response.data.data})
             toast.success(response?.data?.message || "Playlist created successfully");
         } catch (error) {
             console.log("Error fetching playlists: ",error);
             toast.error(error.response?.data?.message || "Error creating the playlist");
-        } finally {
-            set({isFetchingPlaylist: false});
-        }
+        } 
     },
 
     fetchAPlaylist: async (id) => {
@@ -77,14 +69,11 @@ export const usePlaylistStore = create((set)=> ({
 
     deleteAPlaylist: async (id) => {
         try {
-            set({isFetchingPlaylist: true});
             const response = await axiosInstance.delete(`/playlist/${id}`);
             toast.success(response?.data?.message || "Playlist deleted successfully");
         } catch (error) {
             console.log("Error deleting playlists: ",error);
             toast.error(error.response?.data?.message || "Error deleting the playlist");
-        } finally {
-            set({isFetchingPlaylist: false});
-        }
+        }  
     }
 }))
